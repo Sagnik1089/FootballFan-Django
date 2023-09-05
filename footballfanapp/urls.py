936b8a7eafce1c40007bd78f0f1334db6ad440cf
Sagnik1089 <sagnikchakraborty12@gmail.comm>
2023-09-05 15:30:04 +0530

    Minimal Chnages Title and URL

--- footballfanapp/urls.py
+++ footballfanapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('name/<str:name>/<str:age>/',views.name, name='name'),
-    path('clubs/',views.clubs, name='clubs'),
+    path('',views.clubs, name='clubs'),
     path('club/<str:pk>/', views.club, name='club'),
     path('create-club/',views.createfootaballclub, name='create-club'),
     path('update-club/<str:pk>/', views.updatefootaballclub, name='update-club'), 
