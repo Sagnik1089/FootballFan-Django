684879e4311f5d54dc5533f282b8fc8521866e94
Sagnik1089 <sagnikchakraborty12@gmail.comm>
2023-09-20 22:14:21 +0530

    Internation_Implemented

--- footballfanapp/forms.py
+++ footballfanapp/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import footballclub
+from .models import footballclub, internationalteam
 
 class FootballClubForm(ModelForm):
     class Meta:
         model = footballclub
         fields = ['name','logo','official_site','home_venue','slogan','estd','country','league','abb','current_manager','manager_pic','description']
+
+class InternationalForm(ModelForm):
+    class Meta:
+        model = internationalteam
+        fields = ['name','logo','official_site','abb','current_manager','manager_pic','description']
--- footballfanapp/urls.py
+++ footballfanapp/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('create-club/',views.createfootaballclub, name='create-club'),
     path('update-club/<str:pk>/', views.updatefootaballclub, name='update-club'), 
     path('delete-club/<str:pk>/',views.deletefootballclub,name='delete-club'), 
+
+    path('int-teams/',views.int_teams, name='int_teams'),
+    path('int-team/<str:pk>/', views.int_team, name='int_team'),
+    path('create-int-team/',views.createintteam, name='create-int-team'),
+    path('update-int-team/<str:pk>/', views.updateintteam, name='update-int-team'),
+    path('delete-int-team/<str:pk>/',views.deleteintteam,name='delete-int-team'), 
+    
+
 ]
