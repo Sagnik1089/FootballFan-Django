42a9a609a338dfb6d55caa5e00383ee140b750a0
Sagnik1089 <sagnikchakraborty12@gmail.comm>
2023-09-20 19:40:21 +0530

    Update

--- footballfanapp/models.py
+++ footballfanapp/models.py
@@ -7,7 +7,7 @@ class footballclub(models.Model):
     abb=models.CharField(max_length=30, null=True)
     slogan=models.CharField(max_length=200, null=True)
     description=models.TextField(max_length=5000, null=True)
-    logo=models.ImageField(null=True, blank=True, default='default_logo.png')
+    logo=models.ImageField(null=True, blank=True, default='default_logo.png', upload_to='logo/')
     estd=models.IntegerField(help_text='Year')
     country=models.CharField(max_length=30)
     league=models.CharField(max_length=30,null=True)
@@ -15,7 +15,7 @@ class footballclub(models.Model):
     id=models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     home_venue=models.CharField(max_length=100, null=True)
     current_manager=models.CharField(max_length=50,null=True)
-    manager_pic=models.ImageField(null=True, blank=True, default='default_manager_pic.png')
+    manager_pic=models.ImageField(null=True, blank=True, default='default_manager_pic.png', upload_to='managers/')
 
     def __str__(self):
         return self.name
